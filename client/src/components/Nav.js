556c2a6b0385319308d8fccb331a647fcd8fa6bb
amitsinghgh19 <prasadmanigh19>
2019-07-31 22:24:34 -0400

    given border to buttons in navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,14 +4,15 @@ import { Link } from "react-router-dom";
 function Nav() {
   // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
+    <nav className="navbar navbar-expand-lg navbar-light bg-info">
       <Link className="navbar-brand" to="/">
         <span className="blue">G</span>
         <span className="red">o</span>
         <span className="yellow">o</span>
         <span className="blue">g</span>
         <span className="green">l</span>
-        <span className="red">e</span> Books Search
+        <span className="red">e</span>
+        <button type="button" class=" mx-2 btn btn-outline-dark">Search Books</button> 
       </Link>
       <div>
         <ul className="navbar-nav">
@@ -20,11 +21,11 @@ function Nav() {
               to="/saved"
               className={
                 window.location.pathname === "/saved"
-                  ? "nav-link active"
-                  : "nav-link"
+                 
               }
             >
-              Saved Books
+              <button type="button" class="btn btn-outline-dark">Saved Books</button> 
+              
             </Link>
           </li>
         </ul>
